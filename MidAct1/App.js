2678122d326f4ec08df16e767bda320939161fcf
Jotojot22 <jonathanjotojot@gmail.com>
2025-09-25 23:46:50 +0800

    Added MidAct1 branch files into main/MidAct1 folder

---
+++ MidAct1/App.js
@@ -0,0 +1,21 @@
+
+
+import React from 'react';
+import { ScrollView, View, StyleSheet } from 'react-native';
+import Messenger from './Messenger';
+import NewsFeed from './NewsFeed';
+import styles from './styles';
+
+const App = () => {
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+    >
+      <Messenger />
+      <NewsFeed />
+    </ScrollView>
+  );
+};
+
+export default App;
